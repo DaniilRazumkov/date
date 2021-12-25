3cb766c64323db40d133b77283038b69f2085cbd
Daniil <daniilrazumkov9@gmail.com>
2021-12-25 23:04:37 +0500

    date

--- app/src/main/java/com/example/date/MainActivity2.kt
+++ app/src/main/java/com/example/date/MainActivity2.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.View
 import com.example.date.databinding.ActivityMain2Binding
 
 class MainActivity2 : AppCompatActivity() {
@@ -19,6 +20,9 @@ class MainActivity2 : AppCompatActivity() {
         bindingClass.tvResult.text= "Имя:$name"
         bindingClass.tvResult2.text= "Телефон:$number"
     }
-
+fun onClick1(view: View){
+    val i= Intent(this,MainActivity::class.java)
+    startActivity(i)
+}
 
     }
